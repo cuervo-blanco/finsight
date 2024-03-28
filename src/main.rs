9cba1872e96b346ee008ac7257d76d3f690646cd
cuervo-blanco <jaios.228@gmail.com>
2024-03-28 12:11:48 -0400

    initial command commit, program can exit with 'exit' command.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,43 @@
 use std::io::Write;
+use std::process;
 
-fn user_input() {
+fn user_input()
+{
 
     print!("finsight >^º> ");
 
     std::io::stdout().flush().unwrap();
 
-    let mut buffer = String::new();
+    let mut command = String::new();
     let _input = std::io::stdin();
 
-    match _input.read_line(&mut buffer) {
-        Ok(_) => {
-            println!("You typed: {}", buffer.trim());
+    match _input.read_line(&mut command)
+    {
+        Ok(_) =>
+        {
+            match command.trim()
+            {
+                "exit" => {
+                    println!("Exiting");
+                    process::exit(0);
+                },
+                _ => {
+                    println!("Unknown command");
+                }
+            }
         },
-        Err(error) => {
+        Err(error) =>
+        {
             println!("Error getting standard input: {}", error);
         }
     }
 }
 
-fn main() {
-    loop {
+fn main()
+{
+    loop
+    {
         user_input();
-   }
+    }
 }
 
